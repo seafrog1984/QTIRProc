fd2d8270a78d6abe592b6f29226a338789472b24
seafrog1984 <seafrog1984@hotmail.com>
2019-06-27 02:16:04 +0800

    Add Log

--- IRProc/IRProc/main.cpp
+++ IRProc/IRProc/main.cpp
@@ -1,11 +1,14 @@
 #include "IRProc.h"
 #include <QtWidgets/QApplication>
+#include "LogDlg.h"
 
 int main(int argc, char *argv[])
 {
 	QApplication::addLibraryPath("./plugins");
 	QApplication a(argc, argv);
-	IRProc w;
+	//IRProc w;
+	//w.show();
+	LogDlg w;
 	w.show();
 	return a.exec();
 }
